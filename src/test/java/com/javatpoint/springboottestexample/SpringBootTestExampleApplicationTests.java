081bd648c8036e23ad49ad06409f9ca80f86cf5e
niatoned <niatoned@gmail.com>
2023-04-09 15:07:49 -0500

    Update SpringBootTestExampleApplicationTests.java

--- src/test/java/com/javatpoint/springboottestexample/SpringBootTestExampleApplicationTests.java
+++ src/test/java/com/javatpoint/springboottestexample/SpringBootTestExampleApplicationTests.java
@@ -1,7 +1,7 @@
 package com.javatpoint.springboottestexample;
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
-@SpringBootTest(classes = SpringBootTestApplicationExample)
+@SpringBootTest(classes = SpringBootTestExampleApplication)
 class SpringBootTestExampleApplicationTests 
 {
 @Test
